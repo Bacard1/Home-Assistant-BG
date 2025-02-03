03d0c07fb4cd5be130ff5a100b5830f294eb68c1
Bacard1 <87463036+Bacard1@users.noreply.github.com>
2025-02-03 09:52:09 +0100

    Create navigator.language.js

---
+++ Statik/JS/navigator.language.js
@@ -0,0 +1,15 @@
+function setUserLanguage() {
+    let lang = navigator.language || navigator.userLanguage;
+    fetch("/api/states/input_text.user_language", {
+        method: "POST",
+        headers: {
+            "Authorization": "Bearer YOUR_LONG_LIVED_ACCESS_TOKEN",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            state: lang.substring(0, 2) // записва само 'de', 'bg', 'en' и т.н.
+        })
+    });
+}
+
+setUserLanguage();
