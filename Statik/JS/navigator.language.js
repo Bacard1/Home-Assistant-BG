632727838ae57e84403657a1467f929f4bb1f8f5
Bacard1 <87463036+Bacard1@users.noreply.github.com>
2025-02-03 09:54:03 +0100

    Update navigator.language.js

--- Statik/JS/navigator.language.js
+++ Statik/JS/navigator.language.js
@@ -3,7 +3,7 @@ function setUserLanguage() {
     fetch("/api/states/input_text.user_language", {
         method: "POST",
         headers: {
-            "Authorization": "Bearer YOUR_LONG_LIVED_ACCESS_TOKEN",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyMjk1NWJiZGY0YTE0ZTI0ODNlNWNmNGM3ZTY4NTY5NCIsImlhdCI6MTczODU3MjgxMywiZXhwIjoyMDUzOTMyODEzfQ.QIxzjihEC5Gghb2_vr-5CvQuJo-HV34J9ETZ_mNaIE8",
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
